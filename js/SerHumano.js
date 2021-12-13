d4557bed6f1822e7c0d2777d24644b9af10c1b04
Mathluiz23 <matheus_luizs@hotmail.com>
2021-12-12 23:01:31 -0300

    Projeto todo-list

---
+++ js/SerHumano.js
@@ -0,0 +1,26 @@
+class SerHumano {
+
+  peso = 76;
+  altura = 1.75;
+  nome = "Matheus Luiz";
+  filhos = [
+      "Filho 1",
+      "Filho 2",
+      "Filho 3"
+  ];
+
+  comer(comida) {
+
+      alert("O ser humano comeu " + comida);
+
+  }
+
+  andar() {
+
+  }
+
+  correr() {
+
+  }
+
+}
